48d9e8f7eb7df4ca13b3fae571e09e9902ad4aa9
mhwang7758 <2013292420@qq.com>
2021-07-07 16:38:26 +0800

    网络接口

---
+++ netInterface-module/src/main/java/com/qtyx/mhwang/netinterface_module/INetService.java
@@ -0,0 +1,28 @@
+package com.qtyx.mhwang.netinterface_module;
+
+import com.alibaba.android.arouter.facade.template.IProvider;
+
+import java.util.Map;
+
+/**
+ * 公司：广州成林科技信息
+ * 作者：王明海
+ * 时间：7/7/21 4:20 PM
+ * 用途：
+ **/
+public interface INetService extends IProvider {
+    public void init(Map<String,Object> params);
+    public void login(Map<String,Object> params);
+    public void logout(Map<String,Object> params);
+    public void heartBeat(Map<String,Object> params);
+    public void auth(Map<String,Object> params);
+    public void register(Map<String,Object> params);
+    public void qrCode(Map<String,Object> params);
+    public void cardCode(Map<String,Object> params);
+    public void barCode(Map<String,Object> params);
+    public void productDone(Map<String,Object> params);
+    public void stateReport(Map<String,Object> params);
+    public void statusReport(Map<String,Object> params);
+    public void supplyReport(Map<String,Object> params);
+    public void eventReport(Map<String,Object> params);
+}
