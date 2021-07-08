f6d196386a131b2fc963caea6bc578749b6ea896
mhwang7758 <2013292420@qq.com>
2021-07-08 15:17:17 +0800

    1.添加getService接口

---
+++ netInterface-module/src/main/java/com/qtyx/mhwang/netinterface_module/NetServiceUtil.java
@@ -0,0 +1,16 @@
+package com.qtyx.mhwang.netinterface_module;
+
+import com.alibaba.android.arouter.launcher.ARouter;
+
+/**
+ * 公司：广州成林科技信息
+ * 作者：王明海
+ * 时间：7/8/21 3:14 PM
+ * 用途：
+ **/
+public class NetServiceUtil {
+    public static String PATH_SERVICE = "/net/service";
+    public static INetService getService(){
+        return (INetService) ARouter.getInstance().build(PATH_SERVICE).navigation();
+    }
+}
