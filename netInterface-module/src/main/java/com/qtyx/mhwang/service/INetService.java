ebc423eaf61e488cc00dfdfd14b64b16be898add
mhwang7758 <2013292420@qq.com>
2021-07-13 16:54:04 +0800

    1.添加是否登陆接口

--- netInterface-module/src/main/java/com/qtyx/mhwang/service/INetService.java
+++ netInterface-module/src/main/java/com/qtyx/mhwang/service/INetService.java
@@ -12,6 +12,7 @@ import java.util.Map;
  **/
 public interface INetService extends IProvider {
     public boolean connect(Map<String,Object> params);
+    public boolean isLogin();
     public void init(Map<String,Object> params);
     public void login(Map<String,Object> params);
     public void logout(Map<String,Object> params);
